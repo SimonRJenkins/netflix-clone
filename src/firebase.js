5f3b3e3889a92452ad2850838ae4d7a7a5b2b07c
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-08-30 16:53:37 +0100

    Create firebase.js
    Created firebase.js which contains the firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCTlAYH9wn37ixu2zO7lRZYsXeZyLPzPQY",
+  authDomain: "netflix-clone-593d7.firebaseapp.com",
+  projectId: "netflix-clone-593d7",
+  storageBucket: "netflix-clone-593d7.appspot.com",
+  messagingSenderId: "134088428668",
+  appId: "1:134088428668:web:a2f49dedafa6a4be428df9",
+};
