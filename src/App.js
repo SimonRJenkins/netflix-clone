ff0f06d12f5050801a25a1a0da2c94a71ea6facf
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-09-01 23:08:26 +0100

    React router
    Installed react router to project
    Added a router to App.js
    Created screens folder to contain files relating to different screens of the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import {
+  Routes,
+  Route,
+  BrowserRouter
+} from "react-router-dom"
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/test" />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
